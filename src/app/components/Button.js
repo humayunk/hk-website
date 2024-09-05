fb2000185f4bb852ba5d45f73955d7358fb5c31d
humayun khan <humayun.n.k@gmail.com>
2024-09-05 19:01:39 -0400

    fixed button

--- src/app/components/Button.js
+++ src/app/components/Button.js
@@ -16,15 +16,15 @@ export default function Button() {
         onClick={openLink}
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
-        className="bg-yellow-300 px-8 py-4 text-blackfont-mono rounded-xl relative overflow-hidden"
+        className="bg-yellow-300 px-8 py-4 font-regular font-mono rounded-xl relative overflow-hidden"
       >
         <div className={`flex items-center justify-center transition-transform duration-300 ${isHovered ? 'translate-x-full opacity-0' : ''}`}>
-          <span>Book a chat</span>
-          <ArrowRightIcon className="w-5 h-5 ml-2" />
+          <span className="text-black">Book a chat</span>
+          <ArrowRightIcon className="w-5 h-5 ml-2 text-black" />
         </div>
         <div className={`flex items-center justify-center absolute top-0 left-0 w-full h-full transition-transform duration-300 ${isHovered ? 'translate-x-0' : '-translate-x-full'}`}>
-          <ArrowRightIcon className="w-5 h-5 mr-2" />
-          <span>Book a chat</span>
+          <ArrowRightIcon className="w-5 h-5 mr-2 text-black" />
+          <span className="text-black">Book a chat</span>
         </div>
       </button>
     </div>
